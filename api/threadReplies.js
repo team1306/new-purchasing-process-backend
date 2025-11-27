786dc0ed69f2279801a436647f9d8035d5bfa2fb
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-11-27 13:00:13 -0600

    add more logging

--- api/threadReplies.js
+++ api/threadReplies.js
@@ -56,6 +56,7 @@ export default async function handler(req, res) {
 
     const data = await slackRes.json();
 
+    console.log(data)
     if (!data.ok) {
       console.error("Slack API error: (Replies)", data);
       return res.status(500).json({
